dce17a0c92d0ac7de3263f3cdb228c12bba5331d
Jonah Tarver <jonahdevine@gmail.com>
2021-02-09 16:06:17 -0800

    Updated environment variable endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_ENDPOINT: 'https://warm-beyond-62808.herokuapp.com/api'
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT|| 'http://localhost:8000/api'
 }
 
 export default config
